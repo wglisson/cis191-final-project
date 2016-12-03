f9d7a4d454ca54a23a7023ef1a239d9cf3511e1d
wangandr <wangandr@seas.upenn.edu>
2016-12-03 15:55:12 -0500

    finished creating border

--- game.sh
+++ game.sh
@@ -8,13 +8,41 @@ drawBorders() {
 #var 4 is y length
 xprog=0
 yprog=0
-#while [ $xprog -lt 10 ]
-#do 
-tput cup $xprog $2
-echo "X"
-xprog='expr $xprog+1'
-#done
 
+#print the top and bottom borders
+while [[ $xprog -lt $3 ]]
+do 
+xloc=$(( $xprog + $1 ))
+tput cup $2 $xloc
+echo "X"
+#prints bottom border as well
+yloc=$(( $2 + $4 ))
+tput cup $yloc $xloc
+echo "X"
+xprog=$(( $xprog + 1 ))
+done
+
+#print the left and right borders
+while [[ $yprog -lt $4 ]]
+do
+yloc=$(( $yprog + $2 ))
+tput cup $yloc $1
+echo "X"
+xloc=$(( $1 + $3 ))
+tput cup $yloc $xloc
+echo "X"
+yprog=$(( $yprog + 1 ))
+done
+
+xprog=0
+while [[ $xprog -lt $3 ]]
+do
+xloc=$(( $xprog + $1 ))
+yloc = $(( $2 + $4 ))
+tput cup $yloc $xloc
+echo "X"
+xprog=$(( $xprog + 1 ))
+done
 }
 
-drawBorders 10 10 10
+drawBorders 1 1 30 30 
