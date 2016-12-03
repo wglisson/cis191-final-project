5691cf0a31758da3d15c745a84cad546b9ebf192
wangandr <wangandr@seas.upenn.edu>
2016-12-03 01:57:14 -0500

    starting to implement the borders, but need to fix looping. I'll fix it when I'm not falling asleep

--- game.sh
+++ game.sh
@@ -2,5 +2,19 @@
 
 #draw the borders
 drawBorders() {
-#todo
+#var 1 is starting x coordinate
+#var 2 is starting y coordinate
+#var 3 is x length
+#var 4 is y length
+xprog=0
+yprog=0
+#while [ $xprog -lt 10 ]
+#do 
+tput cup $xprog $2
+echo "X"
+xprog='expr $xprog+1'
+#done
+
 }
+
+drawBorders 10 10 10
