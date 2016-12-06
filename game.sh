a10509ceb47dc1ef2d38d7fb570c00d367be0993
wglisson <wfglisson@gmail.com>
2016-12-06 12:43:46 -0500

    Quitting from key command now uses resetTerminal

--- game.sh
+++ game.sh
@@ -575,9 +575,7 @@ startGame() {
                 boardX=$(( boardX + 1 ))
                 updateBoardR
             fi ;;
-        q)  stty echo
-            tput cnorm
-            clear
+        q)  resetTerminal
             exit 0 ;;
         esac
     done
