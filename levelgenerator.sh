2b897720fa8c056c4bee7137ef488cb9e45812a1
Alex Zhao <alexzhao@seas.upenn.edu>
2016-12-06 03:00:39 -0500

    random level generator

--- levelgenerator.sh
+++ levelgenerator.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 
+# #############################################################################
+# Generates a random level (creates a new file)
+# Essentially generates a file of
+#
+# var 1: numRows
+# var 2: numCols
+# #############################################################################
 generateLevel() {
+    numRows=$1
+    numCols=$2
 
+    # create new randomLevel file. Overwrite if exists
+    > randomLevel
 
-
-
+    for row in $(seq 1 $numRows)
+    do
+        for column in $(seq 1 $numCols)
+        do
+            echo -n $(( (RANDOM % 6) )) >> randomLevel
+        done
+        echo "" >> randomLevel
+    done
 }
+
+
+generateLevel 15 33
